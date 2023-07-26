a81daad4936da0444471fea6364ee8b028f6326a
likui628 <likui.it@gmail.com>
2023-07-26 15:24:57 +0800

    chore: setup frontend templates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Nuxt3 Realworld Example App",
+      meta: [
+        {
+          name: "description",
+          content: "Realworld Conduit was built by Nuxt3, Vue3, Quasar and etc",
+        },
+        {
+          name: "keywords",
+          content: "vuejs,nuxt,quasar,realworld,realworld-frontend,vue3,nuxt3",
+        },
+      ],
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic ",
+        },
+      ],
+    },
+  },
+  css: ["@/assets/main.css"],
+});
